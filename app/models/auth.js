a94bc612ceb2709b3e77666f07ded4e8c41dc48b
xyzsince2014 <xyzsince2014@gmail.com>
2020-10-25 18:40:49 +0900

    implemented validation in postUser()

--- app/models/auth.js
+++ app/models/auth.js
@@ -1,6 +1,6 @@
-const mysql = require("mysql");
+const mysql = require('mysql');
 
-const maria = require("../utils/maria");
+const maria = require('../utils/maria');
 
 const dbConfig = {
   host: process.env.DB_HOST,
@@ -10,16 +10,23 @@ const dbConfig = {
 };
 
 const postUser = async user => {
-    const con = mysql.createConnection(dbConfig);
-    try {
-      await maria.beginTransaction(con);
-      await maria.query(con, `INSERT INTO users SET user_id="${user.userId}", user_name="${user.userName}", profile_image_url="${user.profileImageUrl}" ON DUPLICATE KEY UPDATE user_name="${user.userName}", profile_image_url="${user.profileImageUrl}"`);
-      await maria.commit(con);
-    } catch (err) {
-      await maria.rollback(con, err);
-    } finally {
-      con.end();
-    }
+  if (!user.userId || !user.userName) {
+    throw new Error('invalid user');
+  }
+
+  const con = mysql.createConnection(dbConfig);
+  try {
+    await maria.beginTransaction(con);
+    await maria.query(
+      con,
+      `INSERT INTO users SET user_id='${user.userId}', user_name='${user.userName}', profile_image_url='${user.profileImageUrl}' ON DUPLICATE KEY UPDATE user_name='${user.userName}', profile_image_url='${user.profileImageUrl}'`
+    );
+    await maria.commit(con);
+  } catch (err) {
+    await maria.rollback(con, err);
+  } finally {
+    con.end();
+  }
 };
 
 module.exports = {
