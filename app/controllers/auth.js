353b9e0e69d1a007a82fd1d9aeefc1f82aec3431
xyzsince2014 <xyzsince2014@gmail.com>
2021-10-09 20:11:00 +0900

    refactored authenticate()

--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -1,17 +1,12 @@
-const httpStatus = require("http-status-codes");
+const statusCodes = require("http-status-codes");
 
 const authenticate = (req, res) => {
   if(req.isAuthenticated()) {
-    res.status(httpStatus.OK).json({
-      isAuthenticated: true,
-      user: {userId: req.user.userId}
-    });
+    res.status(statusCodes.OK).json({userId: req.user.userId});
     return;
   }
 
-  res.status(httpStatus.FORBIDDEN).json({
-    isAuthenticated: false,
-  });
+  res.status(statusCodes.UNAUTHORIZED).json({userId: ''});
 };
 
 const signout = (req, res) => {
